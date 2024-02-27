cb6a7ac3dab174496c4dd6c727c37dc3a38e055f
Sharyberry <145032663+Sharyberry@users.noreply.github.com>
2024-02-27 22:43:14 +0200

    Added bootstrap to my project

--- src/App.js
+++ src/App.js
@@ -4,27 +4,29 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App </h1>
-      <footer>
-        {" "}
-        This project was created by{" "}
-        <a
-          href="https://www.linkedin.com/in/sharon-mahlakeng-98118531/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Sharon Mahlakeng{" "}
-        </a>{" "}
-        and
-        <a
-          href="https://github.com/Sharyberry/weather-app-react"
-          target="_blank"
-          rel="noreferrer"
-        >
+      <div className="container">
+        <h1>Weather App </h1>
+        <footer>
           {" "}
-          it is open-sourced on Github
-        </a>
-      </footer>
+          This project was created by{" "}
+          <a
+            href="https://www.linkedin.com/in/sharon-mahlakeng-98118531/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Sharon Mahlakeng{" "}
+          </a>{" "}
+          and
+          <a
+            href="https://github.com/Sharyberry/weather-app-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            it is open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
