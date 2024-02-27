4b85de4290597614aba46393f7b27a934930107b
Sharyberry <145032663+Sharyberry@users.noreply.github.com>
2024-02-27 21:58:42 +0200

    Updated the repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,29 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App </h1>
+      <footer>
+        {" "}
+        This project was created by{" "}
+        <a
+          href="https://www.linkedin.com/in/sharon-mahlakeng-98118531/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Sharon Mahlakeng{" "}
+        </a>{" "}
+        and
+        <a
+          href="https://github.com/Sharyberry/weather-app-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          it is open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
