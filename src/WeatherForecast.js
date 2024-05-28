694078a9418784b950307e339be9575d0bf06cb3
Sharyberry <145032663+Sharyberry@users.noreply.github.com>
2024-05-28 14:59:15 +0200

    Refactored code and fixed warnings

---
+++ src/WeatherForecast.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+import "./WeatherForecast.css";
+import axios from "axios";
+import WeatherForecastDay from "./WeatherForecastDay";
+
+export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+
+  function handleResponse(response) {
+    setForecast(response.data.daily);
+    setLoaded(true);
+  }
+
+  function load() {
+    let apiKey = "bd3bb6534458ba51b48c49f5155745b6";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+
+    Axios.length(apiUrl).then(handleResponse);
+  }
+
+  if (loaded) {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          {forecast.map(function (dailyforecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyforecast} />
+                </div>
+              );
+            } else {
+              return null;
+            }
+          })}
+        </div>
+      </div>
+    );
+  } else {
+    load();
+    return null;
+  }
+}
--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,44 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function WeatherTemperature(props) {
-  const [unit, setUnit] = useState("celsius");
-  function showFahrenheit(event) {
-    event.preventDefault();
-    setUnit("Fahrenheit");
-  }
-
-  function showCelcius(event) {
-    event.preventDefault();
-    setUnit("celcius");
-  }
-
-  function Fahrenheit() {
-    return (props.celcius * 9) / 5 + 32;
-  }
-
-  if (unit === "celsius") {
-    return (
-      <div className="WeatherTemperature">
-        <span className="temperature">{Math.round(props.celcius)}</span>
-        <span className="unit">
-          °C|{""}
-          <a href="/" onClick={showFahrenheit}>
-            °F
-          </a>
-        </span>
-      </div>
-    );
-  } else {
-    return (
-      <div className="WeatherTemperature">
-        <span className="temperature">{Math.round(Fahrenheit())}</span>
-        <span className="unit">
-          <a href="/" onClick={showCelcius}>
-            °C
-          </a>
-          {""}| °F
-        </span>
-      </div>
-    );
-  }
+  return (
+    <div className="WeatherTemperature">
+      <span className="temperature">{Math.round(props.celcius)}</span>
+      <span className="unit">°C</span>
+    </div>
+  );
 }
